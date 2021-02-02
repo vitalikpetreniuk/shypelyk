6244c968221734f975c213f4b572617fe7d93874
Marina <marinapetrenyuk@gmail.com>
2021-02-02 14:49:21 +0200

    animated logo

--- js/script.js
+++ js/script.js
@@ -1,27 +1,32 @@
 var shh;
 var hand_l;
 var hand_r;
+var logo;
 
 function mvImg(e,hand) {
-    var valueX = (e.pageX * -1 / 30);
-    var valueY = (e.pageY * -1 / 30);
+    var valueX = (e.pageX * -1 / 50);
+    var valueY = (e.pageY * -1 / 50);
 
-    var valueXr = (e.pageX / 40);
-    var valueYr = (e.pageY / 40);
+    var valueXr = (e.pageX / 80);
+    var valueYr = (e.pageY / 80);
 
-    console.log(valueX);
-    console.log(valueY);
+    var valueXl = (e.pageX / 140);
+    var valueYl = (e.pageY / 140);
 
-    hand_l.style.marginLeft = valueX + "px"
-    hand_l.style.marginBottom = valueY + "px"
+    hand_l.style.marginLeft = valueX + "px";
+    hand_l.style.marginBottom = valueY + "px";
 
-    hand_r.style.marginRight = valueXr + "px"
-    hand_r.style.marginBottom = valueYr + "px"
+    hand_r.style.marginRight = valueXr + "px";
+    hand_r.style.marginBottom = valueYr + "px";
+
+    logo.style.left = valueXl + "px";
+    logo.style.bottom = valueYl + "px";
 }
 window.onload = function() {
     shh = document.getElementById("shh");
     hand_l = document.getElementById("hand-l");
     hand_r = document.getElementById("hand-r");
+    logo = document.getElementById("shh-logo");
 
     if (shh && $(window).width()>1024) {
         shh.addEventListener("mousemove",mvImg,false);
